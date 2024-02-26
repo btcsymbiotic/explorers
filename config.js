0950c2c0b482907c699130e493e64e4f6e5732e4
SCR-NETWORK <scrscrats@gmail.com>
2024-02-26 15:10:11 +0400

    download

---
+++ config.js
@@ -0,0 +1,21 @@
+var api = 'http://node1.cryptosymbiotic.com';
+var apiList = ["http://node1.cryptosymbiotic.com", "https://pool.cryptosymbiotic.com/", "http://node1.cryptosymbiotic.com"];
+var blockTargetInterval = 120;
+var coinUnits = 1000000000;
+var symbol = 'BTCS';
+var refreshDelay = 30000;
+var whiteTheme = "css/themes/white-theme.css";
+var nightTheme = "css/themes/dark-theme.css";
+// pools stats by MainCoins
+var poolsStat = 
+	[
+		["pool.cryptosymbiotic.com", "https://pool.cryptosymbiotic.com/stats"]
+		
+    ];
+var nodesStat = 
+	[
+        ["http://node1.cryptosymbiotic.com"],
+        ["http://node1.cryptosymbiotic.com"],
+        ["https://pool.cryptosymbiotic.com"]
+	
+    ];
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$node = 'http://node1.cryptosymbiotic.com';
+return array(
+	'api' => 'http://node1.cryptosymbiotic.com',
+	'blockTargetInterval' => 120,
+	'coinUnits' => 1000000000,
+	'coinDecimals' => 9
+);
